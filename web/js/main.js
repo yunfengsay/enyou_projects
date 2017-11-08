7f75ac3ec77322b1417aed5c32b46a02c31623c3
yfzhang <fdoctor00@gmail.com>
2017-11-08 19:33:35 +0800

    init

---
+++ web/js/main.js
@@ -0,0 +1,26 @@
+(function($){
+
+
+
+
+    (function ssInit() {
+        
+                ssPreloader();
+                ssFitVids();
+                ssMasonryFolio();
+                ssLightGallery();
+                ssFlexSlider();
+                ssOwlCarousel();
+                ssMenuOnScrolldown();
+                ssOffCanvas();
+                ssSmoothScroll();
+                ssPlaceholder();
+                ssAlertBoxes();
+                ssAnimations();
+                ssIntroAnimation();		
+                ssContactForm();
+                ssAjaxChimp();
+                ssBackToTop();
+        
+            })();
+})(jQuery)
