1234ab9e39654687ea693eafb6de838fe825f295
yfzhang <fdoctor00@gmail.com>
2017-11-08 20:29:33 +0800

    add

--- web/js/main.js
+++ web/js/main.js
@@ -4,23 +4,6 @@
 
 
     (function ssInit() {
-        
-                ssPreloader();
-                ssFitVids();
-                ssMasonryFolio();
-                ssLightGallery();
-                ssFlexSlider();
-                ssOwlCarousel();
-                ssMenuOnScrolldown();
-                ssOffCanvas();
-                ssSmoothScroll();
-                ssPlaceholder();
-                ssAlertBoxes();
-                ssAnimations();
-                ssIntroAnimation();		
-                ssContactForm();
-                ssAjaxChimp();
-                ssBackToTop();
-        
-            })();
+
+    })();
 })(jQuery)
