533961b6b9278330cc3cb78a7a6663745247727e
yunfengsay <yunfengsay@163.com>
2017-12-03 22:31:52 +0800

    脚本

--- run.sh
+++ run.sh
@@ -1,2 +1,6 @@
+kill -9 $(pidof mongod)
+kill -9 $(pidof server_linux_amd64)
+kill -9 $(pidof caddy)
+nohup mongod --auth --dbpath=/usr/local/var/mongodb &
 sudo nohup  ./server/server_linux_amd64 &>server_log.log & 
 sudo nohup caddy &>caddy.log &
