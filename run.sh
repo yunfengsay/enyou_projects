48fe665aba4b4f1e0fd3cfb9495fd51b697e7a92
yunfengsay <yunfengsay@163.com>
2017-12-03 21:13:31 +0800

    run

--- run.sh
+++ run.sh
@@ -1,7 +1,2 @@
-sudo mkdir /etc/caddy
-sudo touch /etc/caddy/Caddyfile
-sudo chown -R root:www-data /etc/caddy
-
-sudo mkdir /etc/ssl/caddy
-sudo chown -R www-data:root /etc/ssl/caddy
-sudo chmod 0770 /etc/ssl/caddy
+sudo nohup  ./server/server_linux_amd64 &>server_log.log & 
+sudo nohup caddy &>caddy.log &
