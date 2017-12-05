55d3a2b3da117a8adf1aa1acf9ff05c2a0dbe3c8
yunfengsay <yunfengsay@163.com>
2017-12-05 17:23:42 +0800

    首页全面滚动

--- web/js/custom.js
+++ web/js/custom.js
@@ -2,11 +2,11 @@
 
   "use strict";
   $("img.lazyload").lazyload({
-    effect : "fadeIn"
+    effect: "fadeIn"
   });
-  
 
-  $("myVideo").each(function() {
+
+  $("myVideo").each(function () {
     var sourceFile = $(this).attr("data-src");
     $(this).attr("src", sourceFile);
     var video = this.parentElement;
@@ -28,7 +28,7 @@
   //     for(var i=0;i<data.data.length;i++){
   //       var v = data.data[i]
   //       $ul.append("<li><a target='_blank' href="+ v.url +">"+v.title+"</a></li>")
-        
+
   //     }
   //   },
   //   error: function(){
@@ -87,15 +87,7 @@
     // set the type of token to animate (available types: 'char' and 'word')
     type: 'word'
   });
-  $(".tabs_item_upslider").unslider({
-    arrows: true,
-    fluid: true,
-    dots: true,
-    keys: true,
-    speed: 500,               //  The speed to animate each slide (in milliseconds)
-    delay: 3000,              //  The delay between slide animations (in milliseconds)
-    complete: function () { },
-  });
+
   // WOW Animation js
   new WOW({ mobile: false }).init();
 
@@ -119,16 +111,98 @@ $(document).ready(function () {
       g.preventDefault();
     });
   })(jQuery);
-
-  $(".unslider-arrow.next").html(">")
-  $(".unslider-arrow.prev").html("<")
-  setInterval(function(){
-     $(".unslider-arrow.next").click()
-  }, 5000)
+  // $(".tabs_item_upslider").unslider({
+  //   arrows: true,
+  //   fluid: true,
+  //   dots: true,
+  //   keys: true,
+  //   speed: 500,               //  The speed to animate each slide (in milliseconds)
+  //   delay: 3000,              //  The delay between slide animations (in milliseconds)
+  //   complete: function () { },
+  // });
+  // $(".unslider-arrow.next").html(">")
+  // $(".unslider-arrow.prev").html("<")
+  // setInterval(function(){
+  //    $(".unslider-arrow.next").click()
+  // }, 5000)
+  $('#dowebok').easyFader({
+    slideDur: 5000
+  })
+  $('#dowebok1').easyFader({
+    slideDur: 5000
+  })
+  // $('#dowebok').pbTouchSlider({
+  //   slider_Wrap: '#dowebokWrap1'
+  // });
+  $("#dowebok img").click(function(){
+    window.open($(this).data("href"))
+  })
   $('a[rel=popover]').popover({
     html: true,
     trigger: 'click',
     placement: $(this).data('placement'),
-    content: function(){return '<img src="'+$(this).data('img') + '" />';}
+    content: function () { return '<img src="' + $(this).data('img') + '" />'; }
   });
+});
+
+jQuery(document).ready(function($){
+	var sliderContainers = $('.cd-slider-wrapper');
+
+	if( sliderContainers.length > 0 ) initBlockSlider(sliderContainers);
+
+	function initBlockSlider(sliderContainers) {
+		sliderContainers.each(function(){
+			var sliderContainer = $(this),
+				slides = sliderContainer.children('.cd-slider').children('li'),
+				sliderPagination = createSliderPagination(sliderContainer);
+
+			sliderPagination.on('click', function(event){
+				event.preventDefault();
+				var selected = $(this),
+					index = selected.index();
+				updateSlider(index, sliderPagination, slides);
+			});
+
+			sliderContainer.on('swipeleft', function(){
+				var bool = enableSwipe(sliderContainer),
+					visibleSlide = sliderContainer.find('.is-visible').last(),
+					visibleSlideIndex = visibleSlide.index();
+				if(!visibleSlide.is(':last-child') && bool) {updateSlider(visibleSlideIndex + 1, sliderPagination, slides);}
+			});
+
+			sliderContainer.on('swiperight', function(){
+				var bool = enableSwipe(sliderContainer),
+					visibleSlide = sliderContainer.find('.is-visible').last(),
+					visibleSlideIndex = visibleSlide.index();
+				if(!visibleSlide.is(':first-child') && bool) {updateSlider(visibleSlideIndex - 1, sliderPagination, slides);}
+			});
+		});
+	}
+
+	function createSliderPagination(container){
+		var wrapper = $('<ol class="cd-slider-navigation"></ol>');
+		container.children('.cd-slider').find('li').each(function(index){
+			var dotWrapper = (index == 0) ? $('<li class="selected"></li>') : $('<li></li>'),
+				dot = $('<a href="#0"></a>').appendTo(dotWrapper);
+			dotWrapper.appendTo(wrapper);
+			var dotText = ( index+1 < 10 ) ? '0'+ (index+1) : index+1;
+			dot.text(dotText);
+		});
+		wrapper.appendTo(container);
+		return wrapper.children('li');
+	}
+
+	function updateSlider(n, navigation, slides) {
+		navigation.removeClass('selected').eq(n).addClass('selected');
+		slides.eq(n).addClass('is-visible').removeClass('covered').prevAll('li').addClass('is-visible covered').end().nextAll('li').removeClass('is-visible covered');
+
+		//fixes a bug on Firefox with ul.cd-slider-navigation z-index
+		navigation.parent('ul').addClass('slider-animating').on('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+			$(this).removeClass('slider-animating');
+		});
+	}
+
+	function enableSwipe(container) {
+		return ( container.parents('.touch').length > 0 );
+	}
 });
